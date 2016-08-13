ed9cb62a9d05472798de6536491e1cf528092947
khaueviana <khaueviana@multconnect.com.br>
2016-08-13 16:38:39 -0300

    inclusão do exemplo do default parameter

--- es6-default-parameter.js
+++ es6-default-parameter.js
@@ -1,9 +1,8 @@
 "use strict"
 
 module.exports = function () {
-
+    
     //Definição de parâmetros default nas definições de funções. 
     let getPrecoFinal = (preco, imposto = 0.7) => preco + preco * imposto;
-    getPrecoFinal(500);
-
+    getPrecoFinal(500); // 850
 }
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-var sampleLet = require('./es6_let.js');
-var sampleConst = require('./es6_const.js');
-var sampleArrowFunction = require('./es6_arrowfunction.js');
+var sampleLet = require('./es6-let.js');
+var sampleConst = require('./es6-const.js');
+var sampleArrowFunction = require('./es6-arrow-function.js');
+var sampleDefaultParameter = require('./es6-default-parameter.js');
 
 //sampleLet();
 
 //sampleConst();
 
-sampleArrowFunction();
+//sampleArrowFunction();
+
+sampleDefaultParameter();
