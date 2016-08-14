3bb3fdd93288d3bc23079ca04d8d393aca0324a8
khaueviana <khaueviana@multconnect.com.br>
2016-08-14 11:01:29 -0300

    melhoria do exemplo do for of em array

--- es6-for-of.js
+++ es6-for-of.js
@@ -2,10 +2,16 @@
 
 module.exports = function () {
 
-    let apelidos = ['A', 'vida', 'não','é','um','toddynho'];
-    apelidos.size = 3;
-    for (let apelido of apelidos) {
-        console.log(apelido);
+    //for...of itera em objetos iteráveis, tipo arrays.
+    let arrayText1 = ['A', 'vida', 'não', 'é', 'um', 'toddynho'];
+    for (let item of arrayText1) {
+        console.log(item);
     }
 
+    //for...in itera sobre todas as propriedades enumeráveis do objeto.
+    let arrayText2 = ['Cara,', 'JS', 'é', 'lindo'];
+    arrayText2.size = 4;
+    for (let item in arrayText2) {
+        console.log(item);
+    }
 }
